08dbc3f8aa898ef24af2087c415da5edcdfda42f
YogSottot <7411302+YogSottot@users.noreply.github.com>
2024-11-02 19:31:03 +0600

    create gh-pages

--- vm_menu/ansible/playbooks/roles/claranet.postgresql/molecule/shared/files/query.sql
+++
@@ -1,5 +0,0 @@
-select version();
--- INSERT INTO table1 (name, age, email) VALUES
--- ('John Doe', 30, 'john.doe@example.com'),
--- ('Jane Smith', 25, 'jane.smith@example.com'),
--- ('Michael Johnson', 40, 'michael.johnson@example.com');
