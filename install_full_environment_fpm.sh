426660b73402c2540e18c1e4b3cd83519199244b
YogSottot <7411302+YogSottot@users.noreply.github.com>
2024-11-03 15:55:06 +0600

    Add experimental astra linux support

--- install_full_environment_fpm.sh
+++ install_full_environment_fpm.sh
@@ -40,7 +40,7 @@ DEST_DIR_MENU="/root"
 FULL_PATH_MENU_FILE="$DEST_DIR_MENU/$DIR_NAME_MENU/menu.sh"
 
 apt update -y
-apt upgrade -y
+apt upgrade -y --enable-upgrade
 apt install -y ansible git locales-all
 # fix for mysql role
 ansible-galaxy collection install 'community.mysql:==3.10.3'
